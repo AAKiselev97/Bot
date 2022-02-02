34ef412474e7fcfaaad97cea194a4e0a2d72db6c
akiselev <AAKiselev97@gmail.com>
2022-02-02 12:21:38 +0300

    add rabbitMQ support for BotApi

--- BotAPI/src/main/java/org/example/bot/api/service/BotApiService.java
+++ BotAPI/src/main/java/org/example/bot/api/service/BotApiService.java
@@ -2,6 +2,7 @@ package org.example.bot.api.service;
 
 import org.example.bot.api.model.telegram.TGChat;
 import org.example.bot.api.model.telegram.TGUser;
+import org.springframework.core.io.InputStreamResource;
 
 public interface BotApiService {
     TGUser getUserStats(String token);
@@ -9,4 +10,8 @@ public interface BotApiService {
     TGChat getChatStats(String token, String chatName);
 
     TGUser getUserStatsByChat(String token, String chatName);
+
+    String formUserHistory(String token);
+
+    InputStreamResource getUserHistory(String token);
 }
